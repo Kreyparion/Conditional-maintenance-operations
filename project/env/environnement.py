d0f32e437f703ea394bbb013e2e1c7e83cf0f34f
Paul Tabbara <paul.tabbara@student-cs.fr>
2022-10-26 18:05:06 +0200

    Add @ for pylint

--- project/env/environnement.py
+++ project/env/environnement.py
@@ -2,25 +2,25 @@ from project.env.actions import Action
 from project.env.states import State
 from typing import List, Dict, Union, Tuple
 
+
 class Environnement:
-    def __init__(self, continuous:bool=False) -> None:
+    def __init__(self, continuous: bool = False) -> None:
         self.continuous = continuous
-        
-    
+
     def reset(self) -> State:
         pass
-    
-    def step(self, action:Action) -> Tuple[State,float,bool]:
-        pass # Pour Etienne, l'appliquer sur state
-    
+
+    def step(self, action: Action) -> Tuple[State, float, bool]:
+        pass  # TODO@Etienne: Pour l'appliquer sur state
+
     def render(self) -> None:
-        pass # Pour Théophile
-    
+        pass  # TODO@Théophile
+
     @classmethod
-    def getPossibleActions(self,state:State)->List[Action]:
+    def getPossibleActions(self, state: State) -> List[Action]:
         return Action.listAction()
-    
-    def getEveryState(self)->List[State]:
+
+    def getEveryState(self) -> List[State]:
         if self.continuous:
             raise RuntimeError("Runtime set to continuous")
         return State.get_states()
