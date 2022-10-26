ce8e6e5f3ad43576906e07035ff51da61200a964
Paul Tabbara <paul.tabbara@student-cs.fr>
2022-10-26 18:59:40 +0200

    :bug: Resolve zip bug in get_states

--- project/env/states.py
+++ project/env/states.py
@@ -9,25 +9,29 @@ class State:
     def __init__(self, continuous: bool, items: List[Item]) -> None:
         self.continuous = continuous
         self.items = items
-        self.wearing_func = items[0].wearing_func
-        self.wear_ranges = [range(item.threshold) for item in self.items]
-        self.max_prods = [item.max_prod for item in self.items]
+        if items:
+            self.wearing_func = items[0].wearing_func
+            if not continuous:
+                self.wear_ranges = [range(item.threshold + 1) for item in self.items]
+                self.max_prods = [item.max_prod for item in self.items]
 
     @classmethod
-    def get_states(self) -> List["State"]:
+    def get_states(
+        self, max_prods: List[float], wear_ranges: List[range], wearing_func: Callable
+    ) -> List["State"]:
         """Method that returns all possible states in case the environment is discrete."""
-        if not self.continuous:
-            return
-        possibilites = product(*self.wear_ranges)
+        possibilities = product(*wear_ranges)
+
         possible_states = [
             State(
+                False,
                 [
-                    Item(i, max_prod, threshold, self.wearing_func)
+                    Item(i, max_prod, threshold, wearing_func)
                     for i, (max_prod, threshold) in enumerate(
-                        self.max_prods, wears_possible
+                        zip(max_prods, wears_possible)
                     )
-                ]
+                ],
             )
-            for wears_possible in possibilites
+            for wears_possible in possibilities
         ]
         return possible_states
