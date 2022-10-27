50f10cffbdf343df86bdc3762954e6c49270aa3a
Paul Tabbara <paul.tabbara@student-cs.fr>
2022-10-27 09:49:51 +0200

    :hammer: Implemented get_state in env

--- project/env/environnement.py
+++ project/env/environnement.py
@@ -1,11 +1,17 @@
+from typing import List, Dict, Union, Tuple
+
 from project.env.actions import Action
 from project.env.states import State
-from typing import List, Dict, Union, Tuple
+from project.env.items import Item
 
 
 class Environnement:
-    def __init__(self, continuous: bool = False) -> None:
+    def __init__(self, items: List[Item], continuous: bool = False) -> None:
         self.continuous = continuous
+        self.items = items
+        if not continuous:
+            for item in self.items:
+                item.threshold = int(item.threshold)
 
     def reset(self) -> State:
         pass
@@ -23,4 +29,9 @@ class Environnement:
     def getEveryState(self) -> List[State]:
         if self.continuous:
             raise RuntimeError("Runtime set to continuous")
-        return State.get_states()
+        max_prods = [item.max_prod for item in self.items]
+        wear_ranges = [range(item.threshold) for item in self.items]
+        wearing_func = self.items[0].wearing_func
+        return State.get_states(
+            max_prods=max_prods, wear_ranges=wear_ranges, wearing_func=wearing_func
+        )
